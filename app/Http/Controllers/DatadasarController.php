31501a0b4099781b25d8384bb33d6295d7ba7901
prov-plalawan <gantengrahfi@gmail.com>
2022-11-25 19:13:31 +0700

    controller

---
+++ app/Http/Controllers/DatadasarController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Datadasar;
+use Illuminate\Http\Request;
+
+class DatadasarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Datadasar  $datadasar
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Datadasar $datadasar)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Datadasar  $datadasar
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Datadasar $datadasar)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Datadasar  $datadasar
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Datadasar $datadasar)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Datadasar  $datadasar
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Datadasar $datadasar)
+    {
+        //
+    }
+}
