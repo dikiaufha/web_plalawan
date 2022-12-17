3a7ffa3c7f019bed543bc6207a7ea3c00bfa425a
Michael David Vinc <tawie18102000@gmail.com>
2022-12-17 21:26:27 +0700

    Login

--- app/Http/Controllers/CaptchaServiceController.php
+++ app/Http/Controllers/CaptchaServiceController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class CaptchaServiceController extends Controller
 {
@@ -14,17 +16,37 @@ class CaptchaServiceController extends Controller
         $credentials = $request->validate([
             'email' => 'required|email:dns',
             'password' => 'required',
-            'captcha' => 'required|captcha'
         ]);
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-            return redirect()->intended('/');
+            if(auth()->user()->role == "admin") {
+                return redirect()->intended('/dashboard');
+            } else {
+                return redirect()->intended('/');
+            }
+
         }
+
+        Alert::error('Error', 'Login Failed!!');
+
+        // if (Auth::attempt($credentials)) {
+        //     $request->session()->regenerate();
+        //     return redirect()->intended('/dashboard');
+        // }
         return back();
     }
 
     public function reloadCaptcha() {
         return response()->json(['captcha'=> captcha_img()]);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/login');
     }
 }
